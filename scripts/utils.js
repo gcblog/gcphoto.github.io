59650be922614c360b63a87974a363bdc8aca485
qianjianeng <329426491@qq.com>
2016-12-04 18:10:19 +0800

    update static  photo

---
+++ scripts/utils.js
@@ -0,0 +1,17 @@
+function request(url) {
+  return new Promise(function (resolve) {
+    var xhr = new XMLHttpRequest()
+    xhr.open('GET', url)
+    xhr.send()
+    xhr.addEventListener('load', function () {
+      resolve(JSON.parse(this.response))
+    })
+  })
+}
+
+function getPhotos(page, source) {
+  page = page || 0
+  source = source || 'so'
+ return request('http://gallery-server.7c00.cc/?source=' + source + '&page=' + page)
+ //return retuest('http://7xr75v.com1.z0.glb.clouddn.com/weixin.jpg')
+}
